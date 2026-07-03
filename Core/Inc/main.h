fd59616ecb2e9b7248135b7c428139bff60ff9d2
HayataSama <amir.khateri135@gmail.com>
2026-07-03 18:15:04 +0330

    chore: init

---
+++ Core/Inc/main.h
@@ -0,0 +1,101 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define BLUE_LED_Pin GPIO_PIN_13
+#define BLUE_LED_GPIO_Port GPIOC
+#define F2_Pin GPIO_PIN_0
+#define F2_GPIO_Port GPIOA
+#define F1_Pin GPIO_PIN_1
+#define F1_GPIO_Port GPIOA
+#define GAS_VALVE_Pin GPIO_PIN_2
+#define GAS_VALVE_GPIO_Port GPIOA
+#define GAS_IND_Pin GPIO_PIN_3
+#define GAS_IND_GPIO_Port GPIOA
+#define PETROL_IND_Pin GPIO_PIN_4
+#define PETROL_IND_GPIO_Port GPIOA
+#define FLASHER_L_Pin GPIO_PIN_5
+#define FLASHER_L_GPIO_Port GPIOA
+#define SW_HORN_Pin GPIO_PIN_10
+#define SW_HORN_GPIO_Port GPIOA
+#define SW_GAS_Pin GPIO_PIN_15
+#define SW_GAS_GPIO_Port GPIOA
+#define SW_HAZARD_Pin GPIO_PIN_3
+#define SW_HAZARD_GPIO_Port GPIOB
+#define SW_FLASHER_L_Pin GPIO_PIN_4
+#define SW_FLASHER_L_GPIO_Port GPIOB
+#define SW_FLASHER_R_Pin GPIO_PIN_5
+#define SW_FLASHER_R_GPIO_Port GPIOB
+#define HORN_Pin GPIO_PIN_6
+#define HORN_GPIO_Port GPIOB
+#define FLASHER_R_Pin GPIO_PIN_7
+#define FLASHER_R_GPIO_Port GPIOB
+#define F4_Pin GPIO_PIN_8
+#define F4_GPIO_Port GPIOB
+#define F3_Pin GPIO_PIN_9
+#define F3_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/gpio.c
@@ -0,0 +1,108 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(BLUE_LED_GPIO_Port, BLUE_LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, F2_Pin|F1_Pin|GAS_VALVE_Pin|PETROL_IND_Pin
+                          |FLASHER_L_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, HORN_Pin|FLASHER_R_Pin|F4_Pin|F3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : BLUE_LED_Pin */
+  GPIO_InitStruct.Pin = BLUE_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(BLUE_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : F2_Pin F1_Pin GAS_VALVE_Pin PETROL_IND_Pin
+                           FLASHER_L_Pin */
+  GPIO_InitStruct.Pin = F2_Pin|F1_Pin|GAS_VALVE_Pin|PETROL_IND_Pin
+                          |FLASHER_L_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : GAS_IND_Pin */
+  GPIO_InitStruct.Pin = GAS_IND_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GAS_IND_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SW_HORN_Pin SW_GAS_Pin */
+  GPIO_InitStruct.Pin = SW_HORN_Pin|SW_GAS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SW_HAZARD_Pin SW_FLASHER_L_Pin SW_FLASHER_R_Pin */
+  GPIO_InitStruct.Pin = SW_HAZARD_Pin|SW_FLASHER_L_Pin|SW_FLASHER_R_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : HORN_Pin FLASHER_R_Pin F4_Pin F3_Pin */
+  GPIO_InitStruct.Pin = HORN_Pin|FLASHER_R_Pin|F4_Pin|F3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
