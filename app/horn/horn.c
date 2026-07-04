a0c4a927ded3d70a89fa5117602d449eab83e103
HayataSama <amir.khateri135@gmail.com>
2026-07-04 19:53:14 +0330

    feat(horn): complete horn module

--- app/horn/horn.c
+++ app/horn/horn.c
@@ -1,9 +1,18 @@
 #include "horn.h"
+#include "battery.h"
 #include "main.h"
+#include <stdint.h>
 
-// Keep track of current switch and LED state
+#define HORN_TIMEOUT (5000 / 10) // 5 seconds with 10ms period
+
+typedef enum { HORN_OFF, HORN_ON, HORN_EXPIRED, HORN_OV, HORN_UV } HornState_t;
+
+// Current horn and switch states
 static GPIO_PinState sw_horn = GPIO_PIN_RESET;
-static HornState_t hornStatus = OFF;
+static HornState_t hornStatus = HORN_OFF;
+
+static HornState_t hornCmd = HORN_OFF;
+static uint16_t hornTimer = 0;
 
 void Horn_Init(void) {
   HAL_GPIO_WritePin(HORN_GPIO_Port, HORN_Pin, GPIO_PIN_SET);
@@ -14,24 +23,75 @@ void Horn_ReadInput(void) {
 }
 
 void Horn_Update(void) {
-  switch (hornStatus) {
-  case OFF:
+  switch (batteryStatus) {
+    /* Check battery voltage */
+  case BATTERY_OV:
+    hornCmd = HORN_OFF;
+    hornStatus = HORN_OV;
     break;
 
-  case ON:
+  case BATTERY_UV:
+    hornCmd = HORN_OFF;
+    hornStatus = HORN_UV;
     break;
 
-  case EXPIRED:
-    break;
+  case BATTERY_OK:
+    /* Horn timeout logic */
+    if (hornTimer >= HORN_TIMEOUT && hornStatus == HORN_ON) {
+      hornCmd = HORN_OFF;
+      hornStatus = HORN_EXPIRED;
+    }
 
-  case OV:
-    break;
+    /* Horn status update */
+    switch (hornStatus) {
+    case HORN_OFF:
+      if (sw_horn == GPIO_PIN_SET) {
+        hornCmd = HORN_ON;
+        hornStatus = HORN_ON;
+        hornTimer = 0;
+      }
+      break;
 
-  case UV:
+    case HORN_ON:
+      hornTimer++;
+      if (sw_horn == GPIO_PIN_RESET) {
+        hornCmd = HORN_OFF;
+        hornStatus = HORN_OFF;
+      }
+      break;
+
+    case HORN_EXPIRED:
+      // Requires a new cmd
+      if (sw_horn == GPIO_PIN_RESET) {
+        hornCmd = HORN_OFF;
+        hornStatus = HORN_OFF;
+      }
+      break;
+
+    case HORN_OV:
+      if (sw_horn == GPIO_PIN_SET) {
+        hornCmd = HORN_ON;
+        hornStatus = HORN_ON;
+      } else {
+        hornCmd = HORN_OFF;
+        hornStatus = HORN_OFF;
+      }
+      break;
+
+    case HORN_UV:
+      // Requires a new cmd
+      if (sw_horn == GPIO_PIN_RESET) {
+        hornCmd = HORN_OFF;
+        hornStatus = HORN_OFF;
+      }
+      break;
+    }
     break;
   }
 }
 
-void Horn_WriteOutput(void) {}
-
-void Horn_Task(void) {}
+void Horn_WriteOutput(void) {
+  // Note: Inverted logic
+  HAL_GPIO_WritePin(HORN_GPIO_Port, HORN_Pin,
+                    hornCmd ? GPIO_PIN_RESET : GPIO_PIN_SET);
+}
--- app/horn/horn.h
+++ app/horn/horn.h
@@ -1,8 +1,6 @@
 #ifndef __HORN_H__
 #define __HORN_H__
 
-typedef enum { OFF, ON, EXPIRED, OV, UV } HornState_t;
-
 void Horn_Init(void);
 void Horn_ReadInput(void);
 void Horn_Update(void);
