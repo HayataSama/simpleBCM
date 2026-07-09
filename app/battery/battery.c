bea6e3c74ed240c03e54eac2ea3bd73ef1e72be9
HayataSama <amir.khateri135@gmail.com>
2026-07-09 20:35:08 +0330

    refactor(battery): refactor battery module

--- app/battery/battery.c
+++ app/battery/battery.c
@@ -1,27 +1,25 @@
 #include "battery.h"
 #include "adc.h"
+#include "util.h"
 #include <stdint.h>
 
 /*
-  Voltage Thresholds:
-  Voltage > 17V -> over-voltage
-  Voltage < 9.2V -> under-voltage
-  Voltage > 9.8V -> under-voltage recovery
+ * Voltage Thresholds:
+ * Voltage > 17V -> OverVoltage
+ * Voltage < 9.2V -> UnderVoltage
+ * Voltage > 9.8V -> UnderVoltage Recovery
+ *
+ * I don't have 17V supply so I divided thresholds by 10: 1.7V, 0.92V and 0.98V
+ * ADC_Value = (Voltage * 2^12) / VDDA
+ */
 
-  I don't have 17V supply so I divided thresholds by 10: 1.7V, 0.92V, 0.98V
-  ADC_Value = (Voltage * 2^12) / VDDA
-*/
-
-#define OV_THRSH 2110   // ~1.7V
-#define UV_THRSH_H 1216 // ~0.98V
-#define UV_THRSH_L 1140 // ~0.92V
-#define VTREND_HYST 5   // ~0.004V
+#define OV_THRSH MV2ADC(1700)  // ~1.7V
+#define UV_THRSH_H MV2ADC(980) // ~0.98V
+#define UV_THRSH_L MV2ADC(920) // ~0.92V
 
 typedef enum { STABLE, FALLING, RISING } VoltageTrend_t;
 
 static uint32_t adcValue;
-static uint32_t adcValuePrev;
-static VoltageTrend_t vTrend = STABLE;
 
 BatteryStatus_t batteryStatus = BATTERY_OK;
 
@@ -30,23 +28,13 @@ void Battery_Init(void) {}
 void Battery_ReadInput(void) { adcValue = adcValues.battery; }
 
 void Battery_Update(void) {
-  // FIXME: This implementation of rising and falling edge is kinda stupid
-  if (adcValue > (adcValuePrev + VTREND_HYST)) {
-    vTrend = RISING;
-  } else if (adcValue < (adcValuePrev - VTREND_HYST)) {
-    vTrend = FALLING;
-  } else {
-    vTrend = STABLE;
-  }
-
   if (adcValue > OV_THRSH) {
     batteryStatus = BATTERY_OV;
-  } else if (adcValue < UV_THRSH_L && vTrend == FALLING) {
+  } else if (adcValue < UV_THRSH_L) {
     batteryStatus = BATTERY_UV;
-  } else if (adcValue > UV_THRSH_H && vTrend == RISING &&
-             batteryStatus == BATTERY_UV) {
+  } else if (adcValue > UV_THRSH_H && batteryStatus == BATTERY_UV) {
     batteryStatus = BATTERY_OK;
-  } else if (batteryStatus != BATTERY_UV) {
+  } else {
     batteryStatus = BATTERY_OK;
   }
 }
