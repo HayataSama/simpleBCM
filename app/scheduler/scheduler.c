31f1d0328efb93c889a9197a70469e3f796bdf16
HayataSama <amir.khateri135@gmail.com>
2026-07-09 20:59:36 +0330

    refactor(scheduler): clean up scheduler and add some docs

--- app/scheduler/scheduler.c
+++ app/scheduler/scheduler.c
@@ -2,19 +2,30 @@
 #include "tim.h"
 #include <stdint.h>
 
-Task_t taskTable[SLOT_COUNT][TASK_PER_SLOT];
-static uint8_t taskIndex[SLOT_COUNT] = {0};
+#define SLOT_COUNT 10
+#define TASK_PER_SLOT 10
+
+typedef struct {
+  void (*func)(void);
+  uint32_t period; // In milliseconds
+  uint32_t count;
+} Task_t;
+
+Task_t taskTable[SLOT_COUNT][TASK_PER_SLOT]; // 2D array of all tasks
+static uint8_t taskIndex[SLOT_COUNT] = {0};  // Number of tasks in each slot
 
 void scheduler(void) {
+  static uint8_t slotIndex = 0;
+
   if (tick) {
     tick = 0;
 
-    static uint8_t slotIndex = 0;
-    Task_t *slotTasks = taskTable[slotIndex];
-    uint8_t taskCount = taskIndex[slotIndex];
+    Task_t *slotTasks = taskTable[slotIndex]; // Select an slot
+    uint8_t taskCnt =
+        taskIndex[slotIndex]; // Number of tasks inside selected slot
 
-    for (uint8_t i = 0; i < taskCount; i++) {
-      Task_t *task = &slotTasks[i];
+    for (uint8_t i = 0; i < taskCnt; i++) {
+      Task_t *task = &slotTasks[i]; // ith task in selected slot
       if (task->func) {
         if (++task->count >= task->period) {
           task->func();
@@ -22,7 +33,7 @@ void scheduler(void) {
         }
       }
     }
-    slotIndex = (slotIndex + 1) % SLOT_COUNT;
+    slotIndex = (slotIndex + 1) % SLOT_COUNT; // Go to next slot
   }
 }
 
@@ -33,6 +44,7 @@ int16_t createTask(void (*func)(void), uint32_t period) {
     slotIndex = (slotIndex + i) % SLOT_COUNT;
 
     if (taskIndex[slotIndex] < TASK_PER_SLOT) {
+      // Create a new task in selectd slot and index
       taskTable[slotIndex][taskIndex[slotIndex]].func = func;
       taskTable[slotIndex][taskIndex[slotIndex]].period = period / SLOT_COUNT;
       taskTable[slotIndex][taskIndex[slotIndex]].count = 0;
--- app/scheduler/scheduler.h
+++ app/scheduler/scheduler.h
@@ -3,15 +3,6 @@
 
 #include <stdint.h>
 
-#define SLOT_COUNT 10
-#define TASK_PER_SLOT 10
-
-typedef struct {
-  void (*func)(void);
-  uint32_t period; // ms
-  uint32_t count;
-} Task_t;
-
 int16_t createTask(void (*func)(void), uint32_t period);
 void scheduler(void);
 
